6a20bba7f58bf7d8b404ed8cb7885f2ff643e976
SSRISANTOSHI <s.satvika2005@gmail.com>
2026-06-07 16:03:15 +0530

    fixed render port

--- backend/app.py
+++ backend/app.py
@@ -139,5 +139,7 @@ def create_app():
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG", "false").lower() == "true", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
