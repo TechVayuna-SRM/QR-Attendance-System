526ff8d3159ca9d7955be2e128e497653cdb13a5
SSRISANTOSHI <s.satvika2005@gmail.com>
2026-06-28 19:34:50 +0530

    Save latest changes before rebasing

--- backend/db.py
+++ backend/db.py
@@ -6,6 +6,7 @@ def get_connection():
         raise RuntimeError("DB_PASSWORD is not set in environment variables")
     return mysql.connector.connect(
         host=Config.DB_HOST,
+        port=Config.DB_PORT,
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         database=Config.DB_NAME
